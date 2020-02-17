a497e534c4aa2a3ba8d0fcf45dcf50824001bcea
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-17 13:17:04 +0100

    Second task

--- 02.01_String/02.01_String/02.01_String/Program.cs
+++ 02.01_String/02.01_String/02.01_String/Program.cs
@@ -15,9 +15,20 @@ namespace _02._01_String
             Console.WriteLine("C");
 
             SubMain("A" + "B" + "C");
+
+            for (int i = 0; i < 10; i++)
+            {
+                RewriteLine(string.Format("Kompliite: {0} %", i * 10 + 10), 200);
+            }
+            Console.WriteLine();
+
+            string text = string.Format("This is newline: {0}", Environment.NewLine);
+            Console.Write(text);
+
         }
+
         /// <summary>
-        /// Write 'A B C'
+        /// Write 'ABC'
         /// </summary>
         /// <param name="args"></param>
         static void SubMain(string args)
@@ -25,6 +36,16 @@ namespace _02._01_String
             Console.WriteLine(args);
         }
 
+        /// <summary>
+        /// Write 'Kompliite: X %' where X is number from 10-100, increasing after <timeout> on the same line
+        /// </summary>
+        /// <param name="text">Text to show</param>
+        /// <param name="timeout">Wait in ms</param>
+        static void RewriteLine(string text, int timeout)
+        {
+            Console.Write(string.Format("\r{0}", text));
+            System.Threading.Thread.Sleep(timeout);
+        }
 
     }
 }
