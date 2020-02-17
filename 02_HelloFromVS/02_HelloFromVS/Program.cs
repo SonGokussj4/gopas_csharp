b87c44d08d9e69eabfcdc0b8a6ce5ddd9f89fbcb
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-17 12:39:25 +0100

    Initial commit

---
+++ 02_HelloFromVS/02_HelloFromVS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02_HelloFromVS
+{
+    class Program
+    {
+        /// <summary>
+        /// Toto je super duper koment
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my padawan!");
+        }
+    }
+}
