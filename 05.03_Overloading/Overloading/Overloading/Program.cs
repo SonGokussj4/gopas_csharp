45f6b0c0274b23467f952c19e72c904776835805
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 15:41:59 +0100

    05.03 Overloading

---
+++ 05.03_Overloading/Overloading/Overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Overloading
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int res1 = Secti(2, 3);
+            int res2 = Secti(2, 3, 4);
+            long res3 = Secti(465464654646465465, 1);
+        }
+
+        private static int Secti(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static long Secti(long a, long b)
+        {
+            return a + b;
+        }
+
+        private static int Secti(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
