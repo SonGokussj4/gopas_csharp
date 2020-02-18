e7563615d3bde644856b4653ba07fd6d2a33b9d2
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 14:52:46 +0100

    05.01 Excercise Methods

---
+++ 05.01_Methods/Methods/Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int res = Secti(a, b);
+            Console.WriteLine("Result: {0}", res);
+
+            Console.WriteLine("Press any key to close this window");
+            Console.Read();
+        }
+
+        private static int Secti(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
