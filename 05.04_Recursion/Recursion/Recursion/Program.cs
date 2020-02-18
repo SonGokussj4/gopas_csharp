f50cd61396e8e91383ae4d9ceb0c7118f8ae0f37
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 15:59:00 +0100

    05.04 Recursion example

---
+++ 05.04_Recursion/Recursion/Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Factorial of number {0} is {1}",
+                number, Factorial(number));
+        }
+
+        private static int Factorial(int currentNumber)
+        {
+            if (currentNumber <= 1)
+                return 1;
+            return currentNumber * Factorial(currentNumber - 1);
+        }
+    }
+}
