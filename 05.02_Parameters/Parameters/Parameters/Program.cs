729ea8efd23759ca306e04660edd2d54fdddffd4
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 15:12:30 +0100

    Output Parameter (ByOut?)

--- 05.02_Parameters/Parameters/Parameters/Program.cs
+++ 05.02_Parameters/Parameters/Parameters/Program.cs
@@ -6,16 +6,20 @@ namespace Parameters
     {
         private static void Main(string[] args)
         {
-            int a = 1;
-            Console.WriteLine("Puvodni: a = {0}", a);
-            Calculate(ref a);
+            //int a = 1;
+            int a;
+            //Console.WriteLine("Puvodni: a = {0}", a);
+            Console.WriteLine("Puvodni: a = neprirazena hodnota");
+            Calculate(out a);
             Console.WriteLine("Potom:   a = {0}", a);
         }
 
         //private static void Calculate(int p)
-        private static void Calculate(ref int p)
+        //private static void Calculate(ref int p)
+        private static void Calculate(out int p)
         {
-            p++;
+            //p++;
+            p = 2;
             Console.WriteLine("Uvnitr:  p = {0}", p);
         }
     }
