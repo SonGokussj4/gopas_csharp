7584c990e28b0d3d6c960c0774542fcb8c7fdff7
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 10:48:21 +0100

    Loopes excercises

--- 04.02_Loops/04.02_Loops/04.02_Loops/Program.cs
+++ 04.02_Loops/04.02_Loops/04.02_Loops/Program.cs
@@ -6,7 +6,54 @@ namespace _04._02_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // WHILE LOOP
+            {
+                int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 10);
+            }
+
+            // FOR LOOP
+            {
+                //int i;
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("For i :" + i);
+                }
+            }
+
+            // FOR LOOP more complex
+            {
+                for (int i = 0, j = 10; i < 10; i++, j--)
+                {
+                    Console.WriteLine("i = {0:C}, j = {1:C}", i, j);
+                }
+            }
+
+            // NESTED FOR LOOP
+            {
+                bool EndCycle = false;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (EndCycle == true) break;
+
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (i == 6) break;
+                        if (i == 7)
+                        {
+                            EndCycle = true;
+                            break;
+                        }
+
+                        Console.WriteLine("i = {0}, j = {1}", i, j);
+                    }
+                }
+            }
+
         }
     }
 }
