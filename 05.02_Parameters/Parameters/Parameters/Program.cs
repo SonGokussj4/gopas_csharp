79160867925191e929f013abd1ec87e2c7b8d300
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 15:08:39 +0100

    05.02 Parameters - ByVal

---
+++ 05.02_Parameters/Parameters/Parameters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parameters
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine("Puvodni: a = {0}", a);
+            Calculate(a);
+            Console.WriteLine("Potom:   a = {0}", a);
+        }
+
+        private static void Calculate(int p)
+        {
+            p++;
+            Console.WriteLine("Uvnitr:  p = {0}", p);
+        }
+    }
+}
