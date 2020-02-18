79769f25d95da13124c45079c729bba55d08ced2
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 12:38:07 +0100

    Better Excercise_loops - optimalized few things

---
+++ 04_Excercise_Loops/Excercise_Loops/Excercise_Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Excercise_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Multiplication of number: ");
+            //string response = Console.ReadLine();
+
+            bool valid = int.TryParse(Console.ReadLine(), out int num);
+
+            if (!valid)
+            {
+                Console.WriteLine("Not a number...");
+                System.Environment.Exit(1);
+            }
+
+            if (num <= 0 || num > 10)
+            {
+                Console.WriteLine("Number is num not between 1-10");
+                System.Environment.Exit(1);
+            }
+            
+            for (int multiplicator = 1; multiplicator <= 10; multiplicator++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", multiplicator, num, multiplicator * num);
+            }   
+        }
+    }
+}
