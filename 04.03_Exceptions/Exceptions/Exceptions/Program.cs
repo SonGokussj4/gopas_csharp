ff8d32d63fdeb75501994bd3ed185a527f64fcf2
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-18 13:41:26 +0100

    Exceptions - propagovana vyjimka

--- 04.03_Exceptions/Exceptions/Exceptions/Program.cs
+++ 04.03_Exceptions/Exceptions/Exceptions/Program.cs
@@ -6,7 +6,42 @@ namespace Exceptions
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Calculate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nPROPAGOVANA VYJIMKA: {0}", ex.Message);
+                Console.WriteLine("DETAIL: {0}", ex.StackTrace);
+            }
+        }
+
+        private static void Calculate()
+        {
+            
+            try
+            {
+                Console.Write("Enter A: ");
+                int numberA = int.Parse(Console.ReadLine());
+                
+                Console.Write("Enter B: ");
+                int numberB = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("{0} / {1} = {2}",
+                    numberA, numberB, numberA / numberB);
+            }
+            catch (System.DivideByZeroException ex)
+            {
+                Console.WriteLine("NEJDE DELIT NULOU: {0}", ex);
+                throw ex;
+            }
+            catch (System.OverflowException ex)
+            {
+                Console.WriteLine("NENI INTEGER: {0}. Pouze od '{1:n}' do '{2:n}'", ex, int.MinValue, int.MaxValue);
+            }
+            Console.WriteLine("Pokracuji...");
+
         }
     }
 }
