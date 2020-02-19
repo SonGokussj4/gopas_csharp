68434dde3e6e16935fcb815d0b916a6a91ff1aee
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 09:27:59 +0100

    06.01.Array

---
+++ 06.01_Array/Array/Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //int[] list;
+            int[] list = new int[4];
+
+            list[0] = 10;
+            list[1] = 20;
+            list[2] = 30;
+            list[3] = 40;
+
+            foreach (int item in list)
+            {
+                Console.WriteLine("Item: {0}", item);
+            }
+        }
+    }
+}
