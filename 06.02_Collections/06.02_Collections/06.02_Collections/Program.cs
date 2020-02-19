2dc8bf951aac49d75a459f899142483d496753c6
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 11:33:41 +0100

    06.02_Collections - ArrayList

---
+++ 06.02_Collections/06.02_Collections/06.02_Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace _06._02_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] list = { 10, 20, 30 };
+            //Console.WriteLine(list.ToString()); 
+
+            ArrayList list = new ArrayList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            list.Remove(20);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
