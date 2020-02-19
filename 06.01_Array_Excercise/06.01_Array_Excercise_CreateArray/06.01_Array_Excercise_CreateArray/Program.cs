65a2e1b9739e6bc7b32de62d0d8943094422c165
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 10:13:44 +0100

    Array Excercise

---
+++ 06.01_Array_Excercise/06.01_Array_Excercise_CreateArray/06.01_Array_Excercise_CreateArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._01_Array_Excercise_CreateArray
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = CreateArray(5, 3);
+
+            foreach (int item in arr)
+            {
+                Console.WriteLine("Value: {0}", item);
+            }
+        }
+
+        private static int[] CreateArray(int size, int defaultValue)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = defaultValue;
+            }
+            return arr;
+        }
+    }
+}
