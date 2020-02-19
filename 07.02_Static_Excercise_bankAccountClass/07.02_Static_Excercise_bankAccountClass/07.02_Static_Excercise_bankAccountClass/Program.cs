851f92192c78fa341b0be0516280dfa4caa369f3
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 15:30:03 +0100

    07.02_Static_Excercise_bankAccountClass - Pridavani uroku a vyuziti staticke metody

---
+++ 07.02_Static_Excercise_bankAccountClass/07.02_Static_Excercise_bankAccountClass/07.02_Static_Excercise_bankAccountClass/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _07._02_Static_Excercise_bankAccountClass
+{
+    class BankAccount
+    {
+        private double Amount;
+        public static double InterestRate = 2;
+
+        public BankAccount(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public void AddInterest()
+        {
+            this.Amount = this.Amount * (BankAccount.InterestRate / 100) + this.Amount;
+        }
+
+        public string GetInfo()
+        {
+            return $"Amount: {this.Amount}, Interest rate: {BankAccount.InterestRate}.";
+        }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BankAccount Fio = new BankAccount(10000);
+            BankAccount MBank = new BankAccount(2000);
+
+            Console.WriteLine("Fio: {0}", Fio.GetInfo());
+            Console.WriteLine("MBank: {0}", MBank.GetInfo());
+
+            Fio.AddInterest();
+            MBank.AddInterest();
+
+            Console.WriteLine("After interest");
+
+            Console.WriteLine("Fio: {0}", Fio.GetInfo());
+            Console.WriteLine("MBank: {0}", MBank.GetInfo());
+
+        }
+    }
+}
