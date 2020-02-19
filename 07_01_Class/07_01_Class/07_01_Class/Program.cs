720234947b405be0ec95d8e52e68c9411309dea1
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 13:49:31 +0100

    07.01_Class - Vytvoreni

---
+++ 07_01_Class/07_01_Class/07_01_Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07_01_Class
+{
+    class Employee
+    {
+
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Employee employee1;
+            //employee1 = new Employee();
+            Employee employee1 = new Employee();
+
+
+        }
+    }
+}
