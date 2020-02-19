f4f7dc9d3621f4b717a109a72f70c0f8eec9fa56
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 13:21:38 +0100

    06.03_Debugging - zkouseni debugu, CallStack je SUPER

---
+++ 06.03_Debugging/06.03_Debugging/06.03_Debugging/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._03_Debugging
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int cislo = Secti(3, 4);
+            int dalsiCislo = cislo - 2;
+
+            Console.WriteLine(dalsiCislo);
+        }
+
+        private static int Secti(int a, int b)
+        {
+            // F9 udelat breakpoint
+            int cisloA = a;
+
+            // Tady to ukaze Call Stack, kde vidim, ze jsem vylezl z metody Main
+            int cisloB = b;
+            int vysledek = cisloA + cisloB;
+            return vysledek;
+        }
+    }
+}
