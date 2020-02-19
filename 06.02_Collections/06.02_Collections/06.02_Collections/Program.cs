377526419e25038130434e7e6d990fffb8d9c695
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 11:39:38 +0100

    06.02_Collections - ArrayList, Generic.List for better type check

--- 06.02_Collections/06.02_Collections/06.02_Collections/Program.cs
+++ 06.02_Collections/06.02_Collections/06.02_Collections/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace _06._02_Collections
 {
@@ -7,20 +8,38 @@ namespace _06._02_Collections
     {
         static void Main(string[] args)
         {
+            // Toto byl stary "Array"
             //int[] list = { 10, 20, 30 };
             //Console.WriteLine(list.ToString()); 
 
-            ArrayList list = new ArrayList();
+            //// NEW ARRAYLIST
+            //ArrayList list = new ArrayList();
+
+            //// JESTE VIC NEW LIST z GENERIC - typove bezpecnejsi
+            List<int> list = new List<int>();
+
             list.Add(10);
             list.Add(20);
             list.Add(30);
 
+            // Odstranim dvacitku
             list.Remove(20);
 
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
+            
+            // Pridam cislo
+            Console.Write("Enter a number: ");
+            list.Add(int.Parse(Console.ReadLine()));  // musel jsem pridat int.Parse, by to str --> int
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
