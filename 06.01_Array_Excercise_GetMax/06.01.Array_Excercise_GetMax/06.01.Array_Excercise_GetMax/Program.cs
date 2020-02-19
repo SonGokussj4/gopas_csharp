35e3fc0a343b1c46deca29fb3559904f9dcc5198
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 10:20:38 +0100

    06.01_Array_Excercise_GetMax

---
+++ 06.01_Array_Excercise_GetMax/06.01.Array_Excercise_GetMax/06.01.Array_Excercise_GetMax/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._01.Array_Excercise_GetMax
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 2, 4, 6, 7, 2, 4, 6 };
+            int result = GetMax(arr);
+
+            Console.WriteLine("Max: '{0}'", result);
+        }
+
+        private static int GetMax(int[] arr)
+        {
+            int semi_max = 0;
+            foreach (int item in arr)
+            {
+                if (item > semi_max) semi_max = item;
+            }
+            return semi_max;
+        }
+    }
+}
