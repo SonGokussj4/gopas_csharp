697aa8874056bbfb7236a0668f8449c55a9ef82c
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-19 09:39:33 +0100

    06.01 Arrays - Complex structures

--- 06.01_Array/Array/Array/Program.cs
+++ 06.01_Array/Array/Array/Program.cs
@@ -2,6 +2,12 @@
 
 namespace Array
 {
+    struct Employee
+    {
+        public string FirstName;
+        public string LastName;
+    }
+
     internal class Program
     {
         private static void Main(string[] args)
@@ -19,6 +25,44 @@ namespace Array
             {
                 Console.WriteLine("Item: {0}", item);
             }
+
+            ////////////////////
+            // KOMPLEXNI POLE //
+            ////////////////////
+
+            Employee[] employees = new Employee[2];
+
+            // Naplnit pole
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.Write("Enter a First Name: ");
+                employees[i].FirstName = Console.ReadLine();
+                Console.Write("Enter a Last Name: ");
+                employees[i].LastName = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("List of Employees");
+            foreach (Employee item in employees)
+            {
+                Console.WriteLine("FirstName: '{0}'; LastName: '{0}'",
+                    item.FirstName, item.LastName);
+            }
+
+            //Enter a First Name: Jan
+            //Enter a Last Name: Verner
+            //Enter a First Name: Petr
+            //Enter a Last Name: Tinka
+
+            //List of Employees
+            //FirstName: 'Jan '; LastName: 'Jan '
+            //FirstName: 'Petr'; LastName: 'Petr'
+
+            ///////////////////////
+            // Vicerozmerne pole //
+            ///////////////////////
+            
+
         }
     }
 }
