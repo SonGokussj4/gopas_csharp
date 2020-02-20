33a6725d8f320f4c2e1a50ab8b19ad832f7cae29
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 10:04:22 +0100

    09.01_Constructors - Prvopocatecni nastaveni

--- 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
+++ 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
@@ -2,11 +2,28 @@
 
 namespace _09._01_Constructors
 {
+    internal class MyClass
+    {
+        public int i;
+    }
+
+    internal class Date
+    {
+        public int Year, Month, Day;
+    }
+
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyClass mc = new MyClass();
+            Console.WriteLine(mc.i);
+
+            Date d = new Date();
+            d.Day = 1;
+            d.Month = 1;
+            d.Year = 1970;
+            Console.WriteLine($"{d.Day:D2}.{d.Month:D2}.{d.Year:D4}");
         }
     }
 }
