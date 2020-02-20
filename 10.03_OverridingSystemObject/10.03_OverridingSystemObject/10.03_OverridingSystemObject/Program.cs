a6c13c0e15ac919d7fc4c09a3aa61ee3221b8576
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 13:54:28 +0100

    10.03_OverridingSystemObject - Vytvoreni zakladni struktury

--- 10.03_OverridingSystemObject/10.03_OverridingSystemObject/10.03_OverridingSystemObject/Program.cs
+++ 10.03_OverridingSystemObject/10.03_OverridingSystemObject/10.03_OverridingSystemObject/Program.cs
@@ -2,11 +2,23 @@
 
 namespace _10._03_OverridingSystemObject
 {
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public Employee(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee employee = new Employee("Ferda", "Mravenec");
+            
         }
     }
 }
