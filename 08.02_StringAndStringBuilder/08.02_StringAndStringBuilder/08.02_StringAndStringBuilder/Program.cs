5fa3a40296aa5b21479fe78bb83593559fd25063
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 09:19:22 +0100

    08.02_StringAndStringBuilder - Prazdny

---
+++ 08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08._02_StringAndStringBuilder
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
