89601d136fc303b9d9272126d4d16c10ab003651
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 10:10:53 +0100

    09.01_Constructors - Parametricky konstruktor

--- 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
+++ 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
@@ -11,11 +11,17 @@ namespace _09._01_Constructors
     {
         public int Year, Month, Day;
         
-        public Date()
+        //public Date()
+        //{
+        //    this.Day = 1;
+        //    this.Month = 1;
+        //    this.Year = 1970;
+        //}
+        public Date(int Day, int Month, int Year)
         {
-            this.Day = 1;
-            this.Month = 1;
-            this.Year = 1970;
+            this.Day = Day;
+            this.Month = Month;
+            this.Year = Year;
         }
     }
 
@@ -26,7 +32,8 @@ namespace _09._01_Constructors
             MyClass mc = new MyClass();
             Console.WriteLine(mc.i);
 
-            Date d = new Date();
+            //Date d = new Date();
+            Date d = new Date(31, 12, 2015);
             Console.WriteLine($"{d.Day:D2}.{d.Month:D2}.{d.Year:D4}");
         }
     }
