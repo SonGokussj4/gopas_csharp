52fdd877ea3aa37f3aef71f480c8f0e1d88abddb
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 09:26:26 +0100

    08.02_StringAndStringBuilder - Pomala iterace pres string

--- 08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/Program.cs
+++ 08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/08.02_StringAndStringBuilder/Program.cs
@@ -6,7 +6,34 @@ namespace _08._02_StringAndStringBuilder
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s1 = "ABC";
+            string s2 = s1;
+
+            // Objekty jsou jeden a tentyz
+            Console.WriteLine(s1 == s2);
+
+            // Toto udela novy objekt, prerusi to referenci
+            s2 += "D";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            // Uz nejsou stejne
+            Console.WriteLine(s1 == s2);
+
+            string s = "";
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            Console.WriteLine("Start");
+            sw.Start();
+            // Slow as fuck - 940 ms (skoro sekunda)
+            for (int i = 0; i < 100000; i++)
+            {
+                s += "A";
+            }
+
+            sw.Stop();
+            Console.WriteLine("Stop");
+            Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms");
         }
     }
 }
