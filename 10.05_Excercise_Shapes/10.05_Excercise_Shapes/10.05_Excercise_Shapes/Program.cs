a67835d8347e2b68e16ebd292c96a6e5500088fa
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 15:58:37 +0100

    10.05_Excercise_Shapes - Nefunkcni zaklad, prvni myslenka

--- 10.05_Excercise_Shapes/10.05_Excercise_Shapes/10.05_Excercise_Shapes/Program.cs
+++ 10.05_Excercise_Shapes/10.05_Excercise_Shapes/10.05_Excercise_Shapes/Program.cs
@@ -2,11 +2,42 @@
 
 namespace _10._05_Excercise_Shapes
 {
+    class ShapeManager
+    {
+        //public static void PrintInvoice(Invoice invoice)
+        //{
+        //    // Zkontroluji, zda invoice vubec existuje. Pokud ne, tak vyskocim z funkce ven.
+        //    if (invoice == null) return;
+
+        //    Console.WriteLine($"ID: {invoice.Id}, TotalPrice: {invoice.TotalPrice}, DateCreated: {invoice.DateCreated}");
+
+        //    // Zapiseme do instance, kde bylo naposled printovano
+        //    invoice.DatePrinted = DateTime.Now;
+        //}
+
+        //public static Invoice MakeInvoice(int id, decimal totalPrice)
+        //{
+        //    Invoice newInvoice = new Invoice();
+        //    newInvoice.Id = id;
+        //    newInvoice.TotalPrice = totalPrice;
+        //    newInvoice.DateCreated = DateTime.Now;
+        //    return newInvoice;
+        //}
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Collections.ArrayList shapes = new System.Collections.ArrayList();
+
+            shapes.Add(ShapeManager.MakeObdelnik(2, 3));
+            shapes.Add(ShapeManager.MakeKvadr(2, 5, 6));
+
+            foreach (var shape in shapes)
+            {
+                shape.ToString();
+            }
         }
     }
 }
