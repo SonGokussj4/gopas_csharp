c9c439db94070bb91f6c99ae678c407a2f277a07
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 15:48:07 +0100

    10.05_Excercise_Shapes - Init

---
+++ 10.05_Excercise_Shapes/10.05_Excercise_Shapes/10.05_Excercise_Shapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10._05_Excercise_Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
