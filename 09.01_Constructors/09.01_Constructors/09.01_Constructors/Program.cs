bd780cbcb308b81258c08b4664dc41d66fe02b61
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 09:59:36 +0100

    09.01_Constructors

---
+++ 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._01_Constructors
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
