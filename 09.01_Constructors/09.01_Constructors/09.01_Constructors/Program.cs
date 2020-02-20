9a25dd1174c483e91606c4bb5c76f64d075b892e
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 10:25:18 +0100

    09.01_Constructors - Constantss in Classes

--- 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
+++ 09.01_Constructors/09.01_Constructors/09.01_Constructors/Program.cs
@@ -11,13 +11,9 @@ namespace _09._01_Constructors
     {
         public int Year, Month, Day;
         public readonly int DaysInMonth;
+        //public const int WeekDayCount = (int)Math.Sqrt(49);  // Nelze pouzit, musi byt konstanta
+        public const int WeekDayCount = 7;  // V kodu bude odkazovano pres 'Date.WeekDaycount', ne instanci
 
-        //public Date()
-        //{
-        //    this.Day = 1;
-        //    this.Month = 1;
-        //    this.Year = 1970;
-        //}
         public Date() : this(1, 1, 1980) { }  // Prazdny body, referuji se ale na jiny konstruktor
         public Date(int Day, int Month, int Year)
         {
@@ -36,10 +32,10 @@ namespace _09._01_Constructors
             Console.WriteLine(mc.i);
 
             Date d = new Date(31, 12, 2015);
-            Console.WriteLine($"{d.Day:D2}.{d.Month:D2}.{d.Year:D4} ... {d.DaysInMonth} days");
+            Console.WriteLine($"{d.Day:D2}.{d.Month:D2}.{d.Year:D4} ... {d.DaysInMonth} days ... DaysInWeek: [{Date.WeekDayCount}]");
             
             Date d2 = new Date();
-            Console.WriteLine($"{d2.Day:D2}.{d2.Month:D2}.{d2.Year:D4} ... {d2.DaysInMonth} days");
+            Console.WriteLine($"{d2.Day:D2}.{d2.Month:D2}.{d2.Year:D4} ... {d2.DaysInMonth} days ... DaysInWeek: [{Date.WeekDayCount}]");
         }
     }
 }
