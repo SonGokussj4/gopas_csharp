88b51f5119bbc009e69888f9e0d5b84d487f644f
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 11:09:32 +0100

    09.02_Finalizace - Spatny pristup, zadny close

--- 09.02_Finalizace/09.02_Finalizace/09.02_Finalizace/Program.cs
+++ 09.02_Finalizace/09.02_Finalizace/09.02_Finalizace/Program.cs
@@ -2,11 +2,24 @@
 
 namespace _09._02_Finalizace
 {
-    class Program
+    class FileStream
     {
-        static void Main(string[] args)
+        public void Open()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Opening file");
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Closing file");
+        }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            FileStream fs = new FileStream();
+            fs.Open();
         }
     }
 }
