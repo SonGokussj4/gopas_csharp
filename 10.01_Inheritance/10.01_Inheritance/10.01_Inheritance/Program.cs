d3fdfe2518475c5956cf98a8f73ecc9eae79acbb
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-20 11:31:41 +0100

    10.01_Inheritance - Init

---
+++ 10.01_Inheritance/10.01_Inheritance/10.01_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10._01_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
