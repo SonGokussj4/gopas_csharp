f0233bcd61d54599a97367fb1e5c4478da13fabe
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-21 14:27:52 +0100

    12.01_Property - Pocatecni funkce

--- 12.01_Property/12.01_Property/12.01_Property/Program.cs
+++ 12.01_Property/12.01_Property/12.01_Property/Program.cs
@@ -2,11 +2,17 @@
 
 namespace _12._01_Property
 {
+    class Fish
+    {
+        public int Weight;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fish fish = new Fish();
+            fish.Weight = -20;
+            Console.WriteLine($"Fish weight: {fish.Weight}");
         }
     }
 }
