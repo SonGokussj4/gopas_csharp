517e9cd10f5e8307a7ab376e5848aa33acff775f
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-21 13:31:43 +0100

    10.08_Interface_Excercise_DLL - Kind of......

---
+++ 10.08_Interface_Excercise_DLL/Gramofon/Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerLibrary;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gramofon gramofon = new Gramofon();
+            gramofon.InsertDesku(new string[] { "Tralala", "Kdepak ty packo" });
+
+        }
+    }
+}
