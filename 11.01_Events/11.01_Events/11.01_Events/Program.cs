17485697d065ea33cbe60daebd2ce6295b8866c7
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-21 13:35:07 +0100

    11.01_Events/ - Init

---
+++ 11.01_Events/11.01_Events/11.01_Events/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11._01_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
