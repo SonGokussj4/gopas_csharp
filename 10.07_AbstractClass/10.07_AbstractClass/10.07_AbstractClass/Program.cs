f89e65db6f619cac6dabed4bce0461c4faa46ab1
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-21 10:34:06 +0100

    10.07_AbstractClass - Init

---
+++ 10.07_AbstractClass/10.07_AbstractClass/10.07_AbstractClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _10._07_AbstractClass
+{
+    class Stream
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream stream = new Stream();
+        }
+    }
+}
