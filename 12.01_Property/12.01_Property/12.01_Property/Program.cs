723ddfc7e966eea05f110eae5c1e49438d47e05f
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-21 14:23:34 +0100

    12.01_Property - Init

---
+++ 12.01_Property/12.01_Property/12.01_Property/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12._01_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
