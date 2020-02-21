a6fbbab423458dbf3289035a8698adf55ee94083
SonGokussj4 <SonGokussj4@centrum.cz>
2020-02-21 10:41:03 +0100

    10.07_AbstractClass - Abstraktni metody a dedeni z abstraktni tridy

--- 10.07_AbstractClass/10.07_AbstractClass/10.07_AbstractClass/Program.cs
+++ 10.07_AbstractClass/10.07_AbstractClass/10.07_AbstractClass/Program.cs
@@ -4,14 +4,31 @@ namespace _10._07_AbstractClass
 {
     abstract class Stream
     {
-
+        public void Close()
+        {
+            Console.WriteLine("Closing Stream");
+        }
+        public abstract void Open();
     }
+
+    class FileStream : Stream
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Opening File Stream");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             // Nelze zkompilovat, protoze z abstraktni classy nejde udelat instance
             //Stream stream = new Stream();
+
+            FileStream fs = new FileStream();
+            fs.Open();
+            fs.Close();
         }
     }
 }
